1291fd8280a6fd7ed0659d64694c2f3a7c529606
josejaviermu <josejaviermu@gmail.com>
2019-08-11 19:00:23 -0500

    no message

---
+++ MyFirstGradle/src/test/java/POM/PruebaGoogle.java
@@ -0,0 +1,18 @@
+package POM;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+//import PruebaGoogle.Library;
+
+public class PruebaGoogle {
+	
+	public class LibraryTest {
+	    @Test public void testSomeLibraryMethod() {
+	        
+	    }
+	}
+
+	
+}
